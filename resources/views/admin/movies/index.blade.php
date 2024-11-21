f0ae081322f688293ef82cf9d46f53bc1c2f803d
AlladaJoshua <allada.personal@gmai.com>
2024-11-21 16:02:12 +0800

    admin table added

--- resources/views/admin/movies/index.blade.php
+++ resources/views/admin/movies/index.blade.php
@@ -1,42 +1,31 @@
 <x-app-layout>
     <div class="admin-container">
         @if ($movies->count())
-            <ul class="movie-poster-container">
+            <table>
+                <tr>
+                    <th>Poster</th>
+                    <th>Title</th>
+                    <th>Release Date</th>
+                    <th>Genre</th>
+                </tr>
                 @foreach ($movies as $movie)
-                    <li class="movie-content">
-                        @if ($movie['poster_path'])
+                    <tr>
+                        <td>
                             <div class="image-container">
                                 <img src="{{ Storage::url('poster_folder/' . $movie['poster_path']) }}"
                                     alt="{{ $movie['title'] }} Poster">
-                                <a href="{{ route('movie.show', $movie->id) }}">
-                                    <div class="overlay">
-                                        <svg class="play-button" width="100" height="100" viewBox="0 0 48 48"
-                                            fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <circle cx="24" cy="24" r="23.5" fill="black"
-                                                fill-opacity="0.3" stroke="white" />
-                                            <path
-                                                d="M37.7883 22.5825C38.9332 23.1811 38.9332 24.8196 37.7883 25.4182L17.3414 36.1094C16.2761 36.6664 15 35.8937 15 34.6915L15 24.0003L15 13.3091C15 12.107 16.2761 11.3342 17.3414 11.8912L37.7883 22.5825Z"
-                                                fill="white" />
-                                        </svg>
-                                    </div>
-                                </a>
                             </div>
-                        @else
-                            <img src="https://via.placeholder.com/200x300?text=No+Image" alt="No Image">
-                        @endif
-
-                        <div class="movie-details">
-                            <div class="movie-title">
-                                <strong>{{ $movie['title'] }}</strong>
-                            </div>
-                            <div class="movie-date">
-                                {{ \Carbon\Carbon::parse($movie['release_date'])->format('Y') }}
-                            </div>
-                        </div>
-                    </li>
+                        </td>
+                        <td>{{ $movie['title'] }}</td>
+                        <td>{{ $movie['release_date'] }}</td>
+                        <td>
+                            @foreach (json_decode($movie->genre_ids, true) as $genre)
+                                <li>{{ $genre['name'] }}</li>
+                            @endforeach
+                        </td>
+                    </tr>
                 @endforeach
-            </ul>
-            {{ $movies->links('vendor.pagination.custom') }}
+            </table>
         @else
             <p>No movies found for @if ($query)
                     "{{ $query }}"
@@ -46,6 +35,5 @@
                     "the selected filter"
                 @endif.</p>
         @endif
-    </div>
     </div>
 </x-app-layout>
